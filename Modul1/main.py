50b07f657b87a4a8d9d80d1f3c713e5077a541a5
C41N1N3 <akhdanalwaafy@gmail.com>
2026-03-26 22:01:57 +0700

    Strukturisasi file Modul 1 dan setup CI/CD

---
+++ Modul1/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+import time
+from datetime import datetime
+
+app = FastAPI()
+
+START_TIME = time.time()
+
+@app.get("/health")
+def health_check():
+    uptime_seconds = time.time() - START_TIME
+    return {
+        "nama": "Muhammad Akhdan Alwaafy",
+        "nrp": "5025241223",
+        "status": "UP",
+        "timestamp": datetime.now().isoformat(),
+        "uptime": f"{uptime_seconds:.2f} seconds"
+    }
